1128e0a94a96cc3836a8661ec8024ae8532210dd
kyungwoh <32881438+kyungwoh@users.noreply.github.com>
2017-11-16 17:05:02 -0800

    HW6

---
+++ Assignment#6/hw6.m
@@ -0,0 +1,106 @@
+% Read CSV file
+data = csvread('hw6_input.csv',1);
+Y = data(:,1); % Activity
+SEQ = data(:,2);
+IR = data(:,3);
+RED = data(:,4);
+Xdata = data(:,5);
+Ydata = data(:,6);
+Zdata = data(:,7);
+
+Fs=50; % Sampling frequency
+Fn=Fs/2; % Nyquist frequency
+T=1/Fs; % Sampling period
+L=length(SEQ); % Length of signal
+
+[b,a] = butter(2,[60/60 180/60]/Fn); % Bandpass Filter (Butterworth)
+[SOS_HR,G_HR] = tf2sos(b,a); % Convert to Second-Order-Section For Stability
+S_HR = filtfilt(SOS_HR,G_HR,IR); % Filter IR To Recover S_HR
+[pksHR,locsHR] = findpeaks(S_HR); % Find peaks
+peakIntervalHR = diff(locsHR);
+HR = 60.*Fs./(peakIntervalHR); % Find Heart Rate
+
+[b,a] = butter(2,[15/60 40/60]/Fn); % Bandpass Filter (Butterworth)
+[SOS_RR,G_RR] = tf2sos(b,a); % Convert to Second-Order-Section For Stability
+S_RR = filtfilt(SOS_RR,G_RR,IR); % Filter IR To Recover S_RR
+[pksRR,locsRR] = findpeaks(S_RR); % Find peaks
+peakIntervalRR = diff(locsRR);
+RR = 60.*Fs./(peakIntervalRR); % Calculate Respiration Rate
+
+S_HRi = S_HR.* (-1); %find min peaks from IR
+[pksHRi,locsHRi] = findpeaks(S_HRi);
+xq = 0:(L-1);
+IR_min_int = interp1(locsHRi,IR(locsHRi),xq,'spline'); %interpolation
+
+[b,a] = butter(2,[60/60 180/60]/Fn); %filter RED
+[SOS,G] = tf2sos(b,a); % Convert to Second-Order-Section For Stability
+S_RED = filtfilt(SOS,G,RED); % Filter RED To Recover S_RED
+[pksRED,locsRED] = findpeaks(S_RED); %max peaks of RED
+
+S_REDi = S_RED.*(-1);
+[pksREDi,locsREDi] = findpeaks(S_REDi); %min peaks of RED
+RED_min_int = interp1(locsREDi,RED(locsREDi),xq,'spline');
+
+IR_max = IR(locsHR);
+IR_min = IR_min_int(locsHR)';
+RED_max = RED(locsHR);
+RED_min = RED_min_int(locsHR)';
+
+AC_IR = IR_max - IR_min;
+AC_RED = RED_max - RED_min;
+DC_IR = IR_min;
+DC_RED = RED_min;
+
+R = AC_RED .* DC_IR ./ AC_IR ./ DC_RED;
+
+figure(3);
+plot(IR,'k');
+hold on;
+plot(RED,'r');
+plot(locsHR,IR_max,'r*');
+plot(locsHR,IR_min,'g*');
+plot(locsHR,RED_max,'r*');
+plot(locsHR,RED_min,'g*');
+
+SPO2 = -45.060.*R.*R + 30.354.*R + 94.845; %calculate SPO2
+
+HRnew(1:L) = HR(1);
+SPO2new(1:L) = SPO2(1);
+for i = 2:length(locsHR)
+    HRnew(locsHR(i-1):locsHR(i)) = HR(i-1);
+    SPO2new(locsHR(i-1):locsHR(i)) = SPO2(i);
+end
+
+RRnew(1:L) = RR(1);
+for i = 2:length(locsRR)
+    RRnew(locsRR(i-1):locsRR(i)) = RR(i-1);
+end
+
+
+window = 10;
+
+SMV = sqrt(Xdata.^2 + Ydata.^2 + Zdata.^2);
+Xmovmean = movmean(Xdata, Fs*window);
+Ymovmean = movmean(Ydata, Fs*window);
+Zmovmean = movmean(Zdata, Fs*window);
+Xmovvar = movvar(Xdata, Fs*window);
+Ymovvar = movvar(Ydata, Fs*window);
+Zmovvar = movvar(Zdata, Fs*window);
+
+X=[IR RED HRnew' RRnew' SPO2new' Xdata Ydata Zdata SMV Xmovmean Ymovmean Zmovmean Xmovvar Ymovvar Zmovvar];
+
+classOrder=unique(Y);
+rng(1); % For reproducibility
+t=templateSVM('Standardize',1);
+CVMdl=fitcecoc(X,Y,'Holdout',0.20,'Learners',t,'ClassNames',classOrder);
+CMdl=CVMdl.Trained{1}; % Extract trained, compact classifier
+testInds=test(CVMdl.Partition);  % Extract the test indices
+XTest=X(testInds,:);
+YTest=Y(testInds,:);
+YTestPredict=predict(CMdl,XTest); % Predict the test-sample labels
+YTestResult=YTest==YTestPredict;
+
+ErrorRate=100 - sum(YTestResult)/length(YTestResult)*100;
+
+T = array2table([IR RED Xdata Ydata Zdata HRnew' RRnew' SPO2new' ones(L,1).*ErrorRate],'VariableNames',{'IR','RED','X','Y','Z','HR','RR','SP02','ERROR_RATE'});
+writetable(T,'team11_assignment6.csv','Delimiter',',','QuoteStrings',true);
---
+++ Assignment#6/main.cpp
@@ -0,0 +1,154 @@
+#include "Wire.h"
+#include "SPI.h"
+#include "MAX30105.h"
+#include "ESP8266WiFi.h"
+#include "ESP8266HTTPClient.h"
+#include "SparkFunLIS3DH.h"
+
+MAX30105 ppgSensor;
+LIS3DH accelSensor(SPI_MODE, 0);
+
+int preCount = 0;
+int count = 0;
+int isEnd = 0;
+long ir[50];
+long red[50];
+float X[50];
+float Y[50];
+float Z[50];
+const char *ssid = "UCInet Mobile Access"; // WiFi settings
+const char* host = "169.234.15.26"; // Server host settings
+const int httpPort = 80;
+const int activityNum = 1;
+  
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Initializing...");
+
+  // Initialize sensor
+  if (!ppgSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
+  {
+    Serial.println("MAX30105 was not found. Please check wiring/power. ");
+    while (1);
+  }
+  Serial.println("Place your index finger on the sensor with steady pressure.");
+
+  ppgSensor.setup();
+
+  uint8_t returnData = 0;
+  returnData = accelSensor.begin();
+  Serial.print("accelSensor returnData=");
+  Serial.println(returnData);
+  if(( returnData != 0x00 )&&( returnData != 0xFF ))
+  {
+	  Serial.println("Problem starting the accel sensor with CS @ Pin 0.");
+  }
+  else
+  {
+	  Serial.println("Accel sensor with CS @ Pin 0 started.");
+  }
+
+  WiFi.begin(ssid); //, password);
+  while (WiFi.status() != WL_CONNECTED){
+      delay(500);
+      Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  // Print the IP address
+  Serial.println(WiFi.localIP());
+}
+
+void loop()
+{
+  long irValue = ppgSensor.getIR();
+  long redValue = ppgSensor.getRed();
+  float xValue = accelSensor.readFloatAccelX();
+  float yValue = accelSensor.readFloatAccelY();
+  float zValue = accelSensor.readFloatAccelZ();
+
+  if(preCount<50*5){
+    preCount++;
+    //don't save
+    Serial.print(preCount);
+    Serial.print(",");
+  }else if(count<50*60*10){
+    //save to array
+    ir[count%50] = irValue;
+    red[count%50] = redValue;
+    X[count%50] = xValue;
+    Y[count%50] = yValue;
+    Z[count%50] = zValue;
+    count++;
+
+    Serial.print("activityNum=");
+    Serial.print(activityNum);
+    Serial.print(", SEQ=");
+    Serial.print(count);
+    Serial.print(", IR=");
+    Serial.print(irValue);
+    Serial.print(", RED=");
+    Serial.print(redValue);
+    Serial.print(", X=");
+    Serial.print(xValue);
+    Serial.print(", Y=");
+    Serial.print(yValue);
+    Serial.print(", Z=");
+    Serial.println(zValue);
+  }else if(isEnd==0){
+    isEnd=1;
+  }else{
+    Serial.print("End.");
+  }
+
+  if(isEnd==0 && count!=0 && count%50==0){
+    //make JSON
+    String postData = "{\"data\":[";
+    for(int i=(count-50); i<count; i++){
+      postData += "[";
+      postData += activityNum;
+      postData += ",";
+      postData += i;
+      postData += ",";
+      postData += ir[i%50];
+      postData += ",";
+      postData += red[i%50];
+      postData += ",";
+      postData += X[i%50];
+      postData += ",";
+      postData += Y[i%50];
+      postData += ",";
+      postData += Z[i%50];
+      postData += "]";
+      if(i!=(count-1)) postData += ",";
+    }
+    postData += "]}";
+    //Serial.println(postData);
+
+    //send to server
+    WiFiClient client;
+    if (!client.connect(host, httpPort)){ //conncect with the host
+      Serial.println("connection failed");
+      return;
+    }
+
+    String request = "POST /hw6.php HTTP/1.1\r\nHost: ";
+    request += host;
+    request += "\r\nAccept: /\r\nContent-Type: application/json\r\nContent-Length: ";
+    request += postData.length();
+    request += "\r\n\r\n";
+    request += postData;
+
+    Serial.print("POST length="); //<=2600 is ok
+    Serial.println(postData.length());
+    client.print(request);
+    
+    while(client.available()>0){
+      String r = client.readStringUntil('\r');
+      Serial.print(r);
+    }
+    if(client.connected()) client.stop();
+    
+  }
+}
